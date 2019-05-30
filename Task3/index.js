d887ec2bb773d19984c2787d18fd543b1e72a451
LovchevaKate <katelovcheva@yandex.by>
2019-05-30 14:40:58 +0300

    added ajax

--- Task3/index.js
+++ Task3/index.js
@@ -1,15 +1,45 @@
-import axios from "axios";
+const appid = "c93e39a59f829c5617a61b1c8819e574";
+const temp = document.getElementById("temp");
+const pressure = document.getElementById("pressure");
+const description = document.getElementById("description");
+const time = document.getElementById("time");
 
-const getWeather = async (city, appid) => {
-  try {
-    const { data } = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appid}`
-    );
-    return data;
-  } catch (e) {
-    console.log(e);
-    return e;
-  }
-};
+let city = document.getElementById("city").value;
 
-export default getWeather;
+function getWeather(city) {
+  let w = [];
+  let t = new Date();
+  time = t.toLocaleString();
+  $.ajax({
+    url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appid}`,
+    type: "GET",
+    success: function(weather) {
+      res = fromKtoC(weather.main.temp);
+      temp.innerHTML = res;
+      pressure.innerHTML = weather.main.pressure;
+      w = weather.weather.pop();
+      description.innerHTML = w.description;
+    },
+    error: function(xhr, ajaxOptions, thrownError) {
+      alert(xhr.status);
+      alert(thrownError);
+    }
+  });
+}
+
+$("#search").click(function(e) {
+  e.preventDefault();
+  getWeather(city);
+});
+
+$("#refresh").click(function(e) {
+  e.preventDefault();
+  getWeather(city);
+});
+
+setTimeout(getWeather(city), 1000);
+
+function fromKtoC(k) {
+  let res = Math.round(Number(k) - 273.15);
+  return res.toString();
+}
