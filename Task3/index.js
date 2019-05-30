a84f284f7b639195466d502d10972c1635dc35f9
LovchevaKate <katelovcheva@yandex.by>
2019-05-30 15:22:42 +0300

    added function refresh

--- Task3/index.js
+++ Task3/index.js
@@ -2,14 +2,15 @@ const appid = "c93e39a59f829c5617a61b1c8819e574";
 const temp = document.getElementById("temp");
 const pressure = document.getElementById("pressure");
 const description = document.getElementById("description");
-const time = document.getElementById("time");
-
-let city = document.getElementById("city").value;
+let time = document.getElementById("time");
+let city = {
+  city : ''
+};
 
 function getWeather(city) {
   let w = [];
   let t = new Date();
-  time = t.toLocaleString();
+  time.innerHTML = t.toLocaleString();
   $.ajax({
     url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appid}`,
     type: "GET",
@@ -19,6 +20,7 @@ function getWeather(city) {
       pressure.innerHTML = weather.main.pressure;
       w = weather.weather.pop();
       description.innerHTML = w.description;
+      
     },
     error: function(xhr, ajaxOptions, thrownError) {
       alert(xhr.status);
@@ -28,16 +30,17 @@ function getWeather(city) {
 }
 
 $("#search").click(function(e) {
-  e.preventDefault();
-  getWeather(city);
+  e.preventDefault();  
+  city.city = document.getElementById("city").value;
+  getWeather(city.city);
 });
 
 $("#refresh").click(function(e) {
-  e.preventDefault();
-  getWeather(city);
+  e.preventDefault(); 
+  getWeather(city.city);
 });
 
-setTimeout(getWeather(city), 1000);
+
 
 function fromKtoC(k) {
   let res = Math.round(Number(k) - 273.15);
