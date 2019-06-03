bdc6f2e61599a93ef8b841ca01538cfd404ed964
LovchevaKate <katelovcheva@yandex.by>
2019-06-03 15:36:01 +0300

    create folder and main files for Task4

---
+++ task4.trello/src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import List from "./List";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <List />
+    </div>
+  );
+}
+
+export default App;
