c80a905065b781ab08c78685473c3d6f7a30fb2c
LovchevaKate <katelovcheva@yandex.by>
2019-06-13 12:59:33 +0300

    create services for cards and comments

--- task4.trello/Trello/Trello.BLL/Services/CardService.cs
+++ task4.trello/Trello/Trello.BLL/Services/CardService.cs
@@ -1,10 +1,98 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using Trello.BLL.Interfaces;
+using Trello.BLL.Models;
+using Trello.DAL.Entities;
+using Trello.DAL.Repositories;
 
 namespace Trello.BLL.Services
 {
-    class CardService
+    class CardService : ICardService
     {
+        UnitOfWork Database { get; set; }
+
+        public CardService(UnitOfWork uof)
+        {
+            Database = uof;
+        }
+
+        [Obsolete]
+        public void CreateCard(CardBLL cardBLL)
+        {
+            if (cardBLL == null)
+            {
+                throw new Exception("Error. card == null");
+            }
+
+            Mapper.Initialize(cfg => cfg.CreateMap<CardBLL, Card>());
+            Card card = Mapper.Map<CardBLL, Card>(cardBLL);
+
+            Database.Cards.Create(card);
+            Database.Save();
+        }
+
+        public void DeleteCard(int id)
+        {
+            if (id == 0)
+                throw new Exception("id == null");
+
+            Database.Cards.Delete(id);
+            Database.Save();
+        }
+
+        [Obsolete]
+        public CardBLL GetCard(int id)
+        {
+            if (id == 0)
+                throw new Exception("id == null");
+
+            var card = Database.Cards.Get(id);
+
+            if (card == null)
+                throw new Exception("card don't find");
+
+            Mapper.Initialize(cfg => cfg.CreateMap<Card, CardBLL>());
+            CardBLL cardBLL = Mapper.Map<Card, CardBLL>(card);
+
+            return cardBLL;
+        }
+
+        [Obsolete]
+        public IEnumerable<CardBLL> GetCards()
+        {
+            var cards = Database.Cards.GetAll();
+            List<CardBLL> cardsBLL = new List<CardBLL>();
+
+            if (cards == null)
+            {
+                throw new Exception("Error create list of cards");
+            }
+
+            foreach (var c in cards)
+            {
+                Mapper.Initialize(cfg => cfg.CreateMap<Card, CardBLL>());
+                CardBLL cardBLL = Mapper.Map<Card, CardBLL>(c);
+
+                cardsBLL.Add(cardBLL);
+            }
+
+            return cardsBLL;
+        }
+
+        [Obsolete]
+        public void UpdateCard(CardBLL cardBLL)
+        {
+            if (cardBLL == null)
+            {
+                throw new Exception("error. update card bll");
+            }
+
+            Mapper.Initialize(cfg => cfg.CreateMap<CardBLL, Card>());
+            Card card = Mapper.Map<CardBLL, Card>(cardBLL);
+
+            Database.Cards.Update(card);
+            Database.Save();
+        }
     }
 }
--- task4.trello/Trello/Trello.BLL/Services/CommentService.cs
+++ task4.trello/Trello/Trello.BLL/Services/CommentService.cs
@@ -1,10 +1,98 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using Trello.BLL.Interfaces;
+using Trello.BLL.Models;
+using Trello.DAL.Entities;
+using Trello.DAL.Repositories;
 
 namespace Trello.BLL.Services
 {
-    class CommentService
+    public class CommentService : ICommentService
     {
+        UnitOfWork Database { get; set; }
+
+        public CommentService(UnitOfWork uof)
+        {
+            Database = uof;
+        }
+
+        [Obsolete]
+        public void CreateComment(CommentBLL commentBLL)
+        {
+            if (commentBLL == null)
+            {
+                throw new Exception("Error. comment == null");
+            }
+
+            Mapper.Initialize(cfg => cfg.CreateMap<CommentBLL, Comment>());
+            Comment comment = Mapper.Map<CommentBLL, Comment>(commentBLL);
+
+            Database.Comments.Create(comment);
+            Database.Save();
+        }
+
+        public void DeleteComment(int id)
+        {
+            if (id == 0)
+                throw new Exception("id == null");
+
+            Database.Comments.Delete(id);
+            Database.Save();
+        }
+
+        [Obsolete]
+        public CommentBLL GetComment(int id)
+        {
+            if (id == 0)
+                throw new Exception("id == null");
+
+            var comment = Database.Comments.Get(id);
+
+            if (comment == null)
+                throw new Exception("comment don't find");
+
+            Mapper.Initialize(cfg => cfg.CreateMap<Comment, CommentBLL>());
+            CommentBLL commentBLL = Mapper.Map<Comment, CommentBLL>(comment);
+
+            return commentBLL;
+        }
+
+        [Obsolete]
+        public IEnumerable<CommentBLL> GetComments()
+        {
+            var comments = Database.Comments.GetAll();
+            List<CommentBLL> commentsBLL = new List<CommentBLL>();
+
+            if (comments == null)
+            {
+                throw new Exception("Error create list of comments");
+            }
+
+            foreach (var c in comments)
+            {
+                Mapper.Initialize(cfg => cfg.CreateMap<Comment, CommentBLL>());
+                CommentBLL commentBLL = Mapper.Map<Comment, CommentBLL>(c);
+
+                commentsBLL.Add(commentBLL);
+            }
+
+            return commentsBLL;
+        }
+
+        [Obsolete]
+        public void UpdateComment(CommentBLL commentBLL)
+        {
+            if (commentBLL == null)
+            {
+                throw new Exception("error. update comment bll");
+            }
+
+            Mapper.Initialize(cfg => cfg.CreateMap<CommentBLL, Comment>());
+            Comment comment = Mapper.Map<CommentBLL, Comment>(commentBLL);
+
+            Database.Comments.Update(comment);
+            Database.Save();
+        }
     }
 }
